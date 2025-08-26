e6b4385a5103f230544cafa88c70ceaaa09682f1
saurabhkr21 <saurabh7221@gmail.com>
2025-08-26 17:29:53 +0530

    ....

--- jobportal/src/app/layout.tsx
+++ jobportal/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
 
-        <ThemeProvider attribute="class" defaultTheme="light" >
+        <ThemeProvider attribute="class" >
         <UserContextProvider>
           <JobProvider>
             <Theme>{children}</Theme>
