2d0a26e3d2ab6d6788877e21b876cab28b56f4bf
saurabhkr21 <saurabh7221@gmail.com>
2025-08-29 11:17:21 +0530

    apply

--- jobportal/src/app/(group)/applied-jobs/page.tsx
+++ jobportal/src/app/(group)/applied-jobs/page.tsx
@@ -1,7 +1,7 @@
+import ApplyDeleteBtn from "@/components/Button/ApplyDeleteBtn";
 import JobCard from "@/components/JobCard";
 import { getUserFromCookies } from "@/helper";
 import prismaClient from "@/services/prisma";
-import { DeleteIcon, Trash, TrashIcon } from "lucide-react";
 
 export default async function page() {
   const user = await getUserFromCookies();
@@ -41,25 +41,24 @@ export default async function page() {
       </div>
     );
   }
-  // const handleDelete = async () => {
-  //   try {
-  //     const res = await fetch("/api/applications/delete", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({ userId: user.id }),
-  //     });
-  //     if (res.ok) {
-  //       // Optionally, you can refresh the page or update the state to reflect the deletion
-  //       window.location.reload();
-  //     } else {
-  //       console.error("Failed to delete applications");
-  //     }
-  //   } catch (error) {
-  //     console.error("Error deleting applications:", error);
-  //   }
-  // };
+  const handleDelete = async () => {
+    try {
+      const res = await fetch("/api/applications/delete", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId: user.id }),
+      });
+      if (res.ok) {
+        window.location.reload();
+      } else {
+        console.error("Failed to delete applications");
+      }
+    } catch (error) {
+      console.error("Error deleting applications:", error);
+    }
+  };
   return (
     <div className="p-8 bg-gray-50 dark:bg-gray-900 min-h-screen">
       <div className="flex flex-col gap-1">
@@ -67,21 +66,20 @@ export default async function page() {
           <h1 className="text-3xl font-bold mb-2 text-gray-800 dark:text-white">
             Applications
           </h1>
-          <button
-          className="h-6 w-6 text-gray-600 dark:text-gray-400 hover:text-red-600 dark:hover:text-red-500 cursor-pointer" 
-          // onClick={handleDelete}
-          >
-            <TrashIcon />
-          </button>
         </div>
         <p className="text-gray-600 dark:text-gray-400 mb-2">
           Here are the jobs you have applied for:
         </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
-        {applications.map((application) => (
-          <JobCard key={application.id} item={application.job} />
-        ))}
+        {applications.map((application) =>
+          application.job ? (
+            <div key={application.id} className="relative flex gap-0.5">
+              <JobCard key={application.id} item={application.job} />
+              <ApplyDeleteBtn job={application.job}  />
+            </div>
+          ) : null
+        )}
       </div>
     </div>
   );
